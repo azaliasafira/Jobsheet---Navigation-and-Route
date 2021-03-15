1ae19c6918458332b40bb503a461b5f762c2ce8c
= <=>
2021-03-15 21:52:14 +0700

    Praktikum

---
+++ lib/pages/item_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:belanja/models/item.dart';
+
+class ItemPage extends StatelessWidget {
+  final Item tempItem;
+
+  ItemPage({Key key, this.tempItem}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Shopping List"),
+          centerTitle: true,
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Text(tempItem.name + " ; " + tempItem.price.toString()),
+        ),
+      ),
+    );
+  }
+}
